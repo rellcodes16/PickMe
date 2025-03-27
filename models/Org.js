92305c90af1d679b6d4da779a6d216351ba539a8
Laurel <devrellmara@gmail.com>
2025-03-27 14:45:31 -0700

    New Commit

--- models/Org.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const OrganizationSchema = new mongoose.Schema({
-    name: { type: String, required: true, unique: true},
-    adminIds: [{type: mongoose.Schema.Types.ObjectId, ref:'User'}]
-})
-
-module.exports = mongoose.model('Organization', OrganizationSchema)
